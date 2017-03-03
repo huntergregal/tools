b2ac23429aab0c010dfb44120119a621b1b9c5c1
huntergregal <huntergregal@gmail.com>
2017-03-03 00:37:44 -0500

    errr body cleanupppp

--- server_hardening/linux/checkPackages.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo 'Requires debsums - apt-get install debsums'
-echo 'Only prints packages that fail the check - May take a while'
-echo 'running...'
-debsums | grep -v "OK$"
--- server_hardening/linux/gitMonDir.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo 'Monitors current directory for any changes using git'
-git init
-git add .
-git commit -m "inital commit"
-clear
-watch -n 10 'git status'
